ad871700ce9b54ccba26bfd3adfe491c0450647c
Peter Abah <peterabah.ob@gmail.com>
2022-01-06 20:44:44 +0100

    Add data module

---
+++ src/data/index.js
@@ -0,0 +1,111 @@
+import NikeAirZoom from './images/nike-air-zoom.jpg';
+import NikeAirMax from './images/nike-air-max.jpg';
+import NewBalanceRedefine from './images/new-balance-redefine.jpg';
+import NikeSpaceHippie from './images/nike-space-hippe.jpg';
+import NikeAirForce from './images/nike-air-force.jpg';
+import AdidasYeezy from './images/adidas-yeezy.jpg';
+import NikeSkepta from './images/nike-skepta.jpg';
+import Converse from './images/converse.jpg';
+import NikeLahar from './images/nike-lahar.jpg';
+import NewBalanceCasablanca from './images/new-balance-casablanca.jpg';
+import NikeISPA from './images/nike-ispa.jpg';
+import AdidasStan from './images/adidas-stan.jpg';
+import NikeZoom from './images/nike-zoom.jpg';
+import NikeACG from './images/nike-acg.jpg'
+
+const data = [
+  {
+    id: 0,
+    name: "Nike Air Zoom",
+    price: 200,
+    image: NikeAirZoom,
+  },
+  {
+    id: 1,
+    name: "Nike Air Max Pre-Day",
+    price: 170,
+    image: NikeAirMax,
+  },
+  {
+    id: 2,
+    name: "New Balance Redefine UX72AA1",
+    price: 230,
+    image: NewBalanceRedefine,
+  },
+  {
+    id: 3,
+    name: "Nike Space Hippe 04",
+    price: 250,
+    image: NikeSpaceHippie,
+  },
+  {
+    id: 4,
+    name: "New Balance Redefine UX72AA1",
+    price: 230,
+    image: NewBalanceRedefine,
+  },
+  {
+    id: 5,
+    name: "Nike Air Force",
+    price: 180,
+    image: NikeAirForce,
+  },
+  {
+    id: 6,
+    name: "Adidas Yeezy 450",
+    price: 350,
+    image: AdidasYeezy,
+  },
+  {
+    id: 7,
+    name: "Nike Skepta Air Max Tailwind V",
+    price: 145,
+    image: NikeSkepta,
+  },
+  {
+    id: 8,
+    name: "Converse Run Star Hike Hi",
+    price: 60,
+    image: Converse,
+  },
+  {
+    id: 9,
+    name: "Nike Lahar Low",
+    price: 180,
+    image: NikeLahar,
+  },
+  {
+    id: 10,
+    name: "New Balance Casablanca",
+    price: 150,
+    image: NewBalanceCasablanca,
+  },
+  {
+    id: 11,
+    name: "Nike ISPA Drifter Gator",
+    price: 155,
+    image: NikeISPA,
+  },
+  {
+    id: 12,
+    name: "Adidas Stan Smith",
+    price: 50,
+    image: AdidasStan,
+  },
+  {
+    id: 13,
+    name: "Nike ZoomX Invincible Run Flyknit",
+    price: 90,
+    image: NikeZoom,
+  },
+  {
+    id: 14,
+    name: "Nike ACG Air Nasu Gore-Tex",
+    price: 200,
+    image: NikeACG,
+  },
+];
+
+const getItems = () => data;
+
+export default getItems;
