626b8a0b9dcc4af10e1ce5505ebdbffa16c10421
Aahil <abdulrahmandomains@gmail.com>
2022-01-08 17:31:17 +0100

    Add styles for ThankYou component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="px-[7.5vw] py-32">
       <h2 className="mb-20 text-5xl text-center">Welcome</h2>
-      <Link className="block w-fit mx-auto text-2xl px-4 py-2 border border-solid" to="/shop">Shop Now </Link>
+      <Link className="block w-fit mx-auto text-2xl px-4 py-2 border-b border-solid" to="/shop">Shop Now </Link>
     </div>
   );
 };
--- src/routes/ThankYou.js
+++ src/routes/ThankYou.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const ThankYou = () => {
-  return <div>Thanks for Shopping</div>;
+  return (
+    <div className="flex flex-col items-center px-[7.5vw] py-20">
+      <h2 className="text-center text-5xl mb-12">Thanks for Shopping</h2>
+      <Link className="text-2xl border-b pb-2 px-2" to="/">Home</Link>
+    </div>
+  );
 };
 
 export default ThankYou;
