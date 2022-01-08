6b7c5a1fdfaea70c0f9a897db52f41bd02624003
Aahil <abdulrahmandomains@gmail.com>
2022-01-08 13:47:19 +0100

    Add style for Home Component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome</h2>
-      <Link to="/shop">Shop Now </Link>
+    <div className="px-[7.5vw] py-32">
+      <h2 className="mb-20 text-5xl text-center">Welcome</h2>
+      <Link className="block w-fit mx-auto text-2xl px-4 py-2 border border-solid" to="/shop">Shop Now </Link>
     </div>
   );
 };
